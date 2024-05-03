8c3e007c709dc53a24e2f80f328518b8e7a80aaf
upomathasin <tahsinchowdhuryupoma@gmail.com>
2024-05-03 23:17:10 +0600

    solved vercel issues2

---
+++ app/categories/[name]/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LoadingCategory() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-5xl text-slate-700">
+        Data is Loading..... <span className="mx-2">||</span> Please wait .
+      </h1>
+    </div>
+  );
+}
